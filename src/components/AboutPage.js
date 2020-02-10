caee0212182021e9efb88d50137fd771ecb13832
watchain <watchain@yahoo.com>
2020-02-10 15:00:12 -0800

    Added Hooks

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+        <a href="/HomePage">Home Page</a>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AboutPage;
